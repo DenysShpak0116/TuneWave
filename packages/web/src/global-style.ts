517d38384440e48f3c012a020fad4ac1181637f4
Artem <118220003+guntersteam@users.noreply.github.com>
2025-05-08 14:27:30 +0300

    fix: fix incorrect font visibility

--- packages/web/src/global-style.ts
+++ packages/web/src/global-style.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
-import MontserratTTF from '@assets/fonts/Montserrat.ttf'
-import Jersey25TTF from '@assets/fonts/Jersey25-Regular.ttf'
+import MontserratTTF from "./fonts/Montserrat.ttf"
+import Jersey25TTF from './fonts/Jersey25-Regular.ttf'
 import { FONTS } from "@consts/fonts.enum";
 import { COLORS } from "@consts/colors.consts";
 
@@ -49,19 +49,15 @@ const GlobalStyle = createGlobalStyle`
       border: none;
   }
 
-    @font-face {
+  @font-face {
     font-family: ${FONTS.MONTSERRAT};
-    src: url('${MontserratTTF}');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC,
-      U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-    font-display: swap;
+    src: url('/fonts/Montserrat.ttf') format('truetype');
+    ont-display: swap;
   }
 
-      @font-face {
+  @font-face {
     font-family: ${FONTS.JERSEY25};
-    src: url('${Jersey25TTF}');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC,
-      U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    src: url('/fonts/Jersey25-Regular.ttf') format('truetype');
     font-display: swap;
   }
 
