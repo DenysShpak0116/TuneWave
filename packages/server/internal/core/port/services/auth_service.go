2d69eede622a4f205fbcf4d1c36206847b3a8c83
DenysShpak0116 <shpak.denys.oleks@gmail.com>
2025-07-05 13:21:58 +0300

    test: add password recovery handler tests

--- packages/server/internal/core/port/services/auth_service.go
+++ packages/server/internal/core/port/services/auth_service.go
@@ -2,9 +2,7 @@
 
 package services
 
-import "github.com/DenysShpak0116/TuneWave/packages/server/internal/adapter/httpserver/handlers/dto"
-
 type AuthService interface {
-	HandleForgotPassword(req dto.ForgotPasswordRequest) (string, error)
-	HandleResetPassword(req dto.ResetPasswordRequest) error
+	HandleForgotPassword(email string) (string, error)
+	HandleResetPassword(token, newPassword string) error
 }
